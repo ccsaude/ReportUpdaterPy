b31531a5924dcbfcdda717676c960f44da7ebece
Agnaldo Samuel <agnaldosamuel3@gmail.com>
2018-11-01 09:45:25 -0500

    UI for keygen

--- keygen.py
+++ keygen.py
@@ -1,12 +1,46 @@
 
 import onetimepad
-import sys
+from appJar import gui
+import os.path
 
 
-filename = sys.argv[1]
-if filename.endswith(".sql"):
-    cipher = onetimepad.encrypt(filename, 'randomkey')
-    print("A chave e: " + cipher)
-else:
-    print("parametro tem que ser ficheiro sql")
+# create a GUI variable called app                                                          # ##
+app = gui()                                                                                 # ##
+app.addLabel("title", "Keygen for OpenMRS update")                                          # ##
+app.addFileEntry("ficheiro")                                                                # ##
+app.setEntry("ficheiro", "selecionar ficheiro", callFunction=True)                          # ##
+app.addTextArea("chave")                                                                    # ##
+app.setGeometry(300, 300)                                                                   # ##
+# #############################################################################################
 
+
+def press(button):
+
+    if button == "generate":
+        filePath = app.getEntry("ficheiro").rstrip()
+        app.clearTextArea("chave", callFunction=True)
+        print(filePath)
+        if not filePath.endswith(".sql"):
+            app.infoBox("Erro", "Ficheiro errado", parent=None)
+            app.clearTextArea("chave", callFunction=True)
+
+        else:
+            if os.path.isfile(filePath):
+                pos = filePath.rfind("/")
+                filename = filePath[pos + 1:]
+                print(filename)
+                cipher = onetimepad.encrypt(filename, 'randomkey')
+                app.setTextArea("chave", cipher+ '\n', end=True, callFunction=True)
+            else:
+                app.infoBox("Erro", "Ficheiro nao encontrado", parent=None)
+                app.clearTextArea("output", callFunction=True)
+
+
+def main():
+    app.addButtons(["generate"], press)
+    # app.setTextAreaOverFunction("ficheiro", [enter, leave])
+    app.go()
+
+
+if __name__ == "__main__":
+    main()
